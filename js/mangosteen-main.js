97243b2c4c91d3f3ff3db207e16ce4ec6f951fc5
Constance Duong <constancegd@gmail.com>
2014-07-29 16:15:21 -0700

    add dark CSS for WP

--- js/mangosteen-main.js
+++ js/mangosteen-main.js
@@ -40,7 +40,7 @@ function search(search_query) {
 				formatted_html_email += "<div class='sender'><span class='sender-name'>" + email.from.name + "</span><span class='sender-email'> &#60" + email.from.email + "&#62</span></div>";
 				formatted_html_email += "<div class='subject'>" + email.subject + "</div>";
 				formatted_html_email += "<div class='preview'>" + email.body_preview + "</div>";
-				formatted_html_email += "<div class='time'>" + email.sent_time + "</div>";
+				formatted_html_email += "<div class='time'>Received " + email.sent_time + "</div>";
 				formatted_html_email += "</div>";
 				formatted_html_email_set += formatted_html_email;
 			}
