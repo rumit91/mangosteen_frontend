55373a38dbf94a36e7967fc56cf6c8c4f83b0485
Timur Aleshin <timur.aleshin@outlook.com>
2014-07-29 12:56:34 -0700

    no message

--- js/mangosteen-main.js
+++ js/mangosteen-main.js
@@ -1,5 +1,6 @@
-//var json_endpoint = "http://eql.herokuapp.com/fake";
-var json_endpoint = "test.json";
+var json_endpoint = "http://eql.herokuapp.com/fake";
+var json_terms_endpoint = "http://eql.herokuapp.com/terminals";
+//var json_endpoint = "test.json";
 $(document).ready(function(){
 	
 	$(document).keypress(function(e) {
@@ -13,20 +14,22 @@ $(document).ready(function(){
 function search(search_query) {
 	alert(search_query);
 	$.getJSON( json_endpoint, function( data ) {
-		var server_response = $.parseJSON( data );
+		//var server_response = $.parseJSON( data );
+		var server_response = data;
 		if(server_response.result.parse_success) {		
-			var formatted_email_set = [];
-			for(var email in server_response.emails)
+			var formatted_html_email_set = "";
+			for(var array_id in server_response.emails)
 			{
-				var formatted_email = "<ul>";
-				formatted_email += "<li>" + email.from.name + " &#60" + email.from.email + "&#62</li>";
-				formatted_email += "<li>" + email.subject + "</li>";
-				formatted_email += "<li>" + email.body_preview + "</li>";
-				formatted_email += "<li>" + email.sent_time + "</li>";
-				formatted_email += "</ul>";
-				formatted_email_set[formatted_email_set.length] = formatted_email;
+				var email = server_response.emails[array_id];
+				var formatted_html_email = "<ul>";
+				formatted_html_email += "<li>" + email.from.name + " &#60" + email.from.email + "&#62</li>";
+				formatted_html_email += "<li>" + email.subject + "</li>";
+				formatted_html_email += "<li>" + email.body_preview + "</li>";
+				formatted_html_email += "<li>" + email.sent_time + "</li>";
+				formatted_html_email += "</ul>";
+				formatted_html_email_set += formatted_html_email;
 			}
-			$("#email-container").append(formatted_emails);
+			$("#email-container").append(formatted_html_email_set);
 		}
 	});
 }
