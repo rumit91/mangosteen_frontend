16617f0a1c85a724a515c1f65a5bda4ad75a1802
Samir Ahmed <samirahmed2013@gmail.com>
2014-08-11 18:09:33 -0700

    update tree

--- js/autocomplete_tree.js
+++ js/autocomplete_tree.js
@@ -1,4 +1,7 @@
+params = {}
+
 var snapmeetPeopleUrl = "https://snapmeet.azurewebsites.net/People/Search"
+var mangosteen_contacts_endpoint = "http://eql.herokuapp.com/contacts/";
 
 function launch_word(){
   window.location.href = "https://office.live.com/start/Word.aspx?omkt=en%2DUS";
@@ -51,6 +54,17 @@ function search_the_web(raw){
   window.location.href= ("http://bing.com/search?q=" + query)
 }
 
+function mangosteen_people_search(prefix, onSuccess){
+  $.get(mangosteen_contacts_endpoint + prefix, function(data) {
+    if (data) {
+      onSuccess(data);
+    }
+    else{
+      onSuccess([]);
+    }
+  });
+}
+
 function snapmeet_people_search( prefix , onSuccess ){
   $.get(snapmeetPeopleUrl + "?user=saahm&query="+ prefix , function(data){ 
           var names = []
@@ -62,13 +76,63 @@ function snapmeet_people_search( prefix , onSuccess ){
         }); 
 }
 
-params = {}
+function fast_search(query){
+  window.location.href = "https://msft.spoppe.com/search/Pages/results.aspx?k=" + query;
+}
+
+function show_me_emails(search_query) {
+  console.log("Raw Query " + search_query);
+  search_query = search_query.lower();
+  if (search_query.indexOf("show me emails")){
+    search_query = search_query.substring("show me emails".length);
+  }
+	
+  console.log("Searching for " + search_query);
+	
+  $("#email-container").empty();
+	$.getJSON( json_endpoint + search_query, function( data ) {
+		//var server_response = $.parseJSON( data );
+		var server_response = data;
+		if(server_response.result.parse_success) {	
+			showParsingAndTime(server_response.result, server_response.parse_terms);
+			//var formatted_html_email_set = "";
+			for(var array_id in server_response.emails)
+			{
+				var email = server_response.emails[array_id];
+				var formatted_html_email = '<div class="email-result">';
+				var date_time = new Date(email.sent_time);
+			
+        formatted_html_email += "<div class='email-result-left'>";
+						formatted_html_email += "<div class='sender phone'><span class='sender-name phone'>" + email.from.name + "</span></div>";
+						formatted_html_email += "<div class='subject phone'>" + shortenText(email.subject, true) + "</div>";
+						formatted_html_email += "<div class='preview phone'>" + shortenText(email.body_preview, true) + "</div>";
+					formatted_html_email += "</div>";
+						formatted_html_email += "<div class='email-result-right'>";
+						formatted_html_email += "<div class='date phone'>" + getDateDisplayString(date_time) + "</div>";
+						formatted_html_email += "<div class='time phone'>" + getTimeDisplayString(date_time) + "</div>";
+					formatted_html_email += "</div>";
+      }
+    }
+  });
+}
+
+function reset_tree(){
+  params = {}
+  // todo add other things here ...  as needed
+}
 
 tree = {
   "show me emails":{ 
-    "execute" : function(){ /*hit mangosteen */ } ,
+    "execute" : function( text ){ search(text) } ,
     "options":{ 
-      "from" : {"autcomplete":true, "action" : null},
+      "to" : {
+        "autcomplete": mangosteen_people_search, 
+        "action" : null
+      },
+      "from" : {
+        "autcomplete": mangosteen_people_search, 
+        "action" : null
+      },
       "about" : { "terms": ["office now", "digital life & digital work"], "action" : null },
       "with links to" : { "terms" : ["yammer.com", "facebook.com"], "action":null},
       "sent before " : {"terms" : ["yesterday", "monday", "june"], "action": null},
@@ -94,18 +158,31 @@ tree = {
     }
   },
   "schedule a meeting": {
-    "execute": null,
+    "execute": function(){ /* TODO */},
     "options" : {
-      "with" : { "autocomplete" : snapmeet_people_search, },
-      "sometime" : { "terms" : ["this week", "today", "tomorrow"]
+      "with" : { 
+        "autocomplete" : snapmeet_people_search,
+        "action" : function(person){ params.person = person; }
+      },
+      "sometime" : { 
+        "terms" : ["this week", "today", "tomorrow"],
+        "action" : function(time){ params.time = time; }
+      },
+      "about" : { 
+        "action" : function(subject){ params.time = subject; },
+        "terms": ["office now", "one clip", "next", "loud mango", "revolve", "hiking", "bing ux", "DLW"] 
       }
     }
   },
   "find me documents":{ 
     "execute": function(){ 
+      params = params || {};
       if (params.person && !params.about){
         find_documents_by(params.email);
-      } 
+      }
+      if (params.about){
+        fast_search(params.about + " " + (params.person || "" ) )
+      }
     },
     "options":{
       "by": {
@@ -137,6 +214,6 @@ tree = {
     }
   },
   "search the web":{
-    "execute": function(text){ search_the_web(text); },
-  },
+    "execute": function(text){ search_the_web(text); }
+  }
 }
